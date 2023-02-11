5bcd6fd4013ee1c4121d774dff701111f4b0a714
SameerJadav <sameerjadav010@gmail.com>
2023-02-11 09:13:12 +0530

    Added slug page

---
+++ app/[slug]/page.tsx
@@ -0,0 +1,52 @@
+import { notFound } from 'next/navigation';
+import { allPosts } from 'contentlayer/generated';
+
+export async function generateStaticParams() {
+  return allPosts.map((post) => ({
+    slug: post.slug,
+  }));
+}
+
+export async function generateMetadata(params: { slug: string }) {
+  const post = allPosts.find((post) => post.slug === params.slug);
+
+  if (!post) {
+    return;
+  }
+
+  const { title, date, description } = post;
+
+  return {
+    title,
+    description,
+    openGraph: {
+      title,
+      description,
+      type: 'article',
+      date,
+    },
+    twitter: {
+      card: 'summary_large_image',
+      title,
+      description,
+    },
+  };
+}
+
+export default async function Blog(params: { slug: string }) {
+  const post = allPosts.find((post) => post.slug === params.slug);
+
+  if (!post) {
+    notFound();
+  }
+
+  return (
+    <section>
+      <h1>{post.title}</h1>
+      <div>
+        <div>{post.date}</div>
+        <div />
+      </div>
+    </section>
+  );
+}
