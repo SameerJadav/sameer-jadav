16c4f080f0ce0e0d196ae4efffe44b122b15f0a9
SameerJadav <sameerjadav010@gmail.com>
2023-02-11 18:12:21 +0530

    Added new font for h1

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -31,13 +31,9 @@ export default async function Blog({ params }) {
 
   return (
     <section>
-      <h1 className="font-bold text-3xl font-serif max-w-[650px]">
-        {post.title}
-      </h1>
-      <div className="grid grid-cols-[auto_1fr_auto] items-center mt-4 mb-8 font-mono text-sm max-w-[650px]">
-        <div className="bg-neutral-100 dark:bg-neutral-800 rounded-md px-2 py-1 tracking-tighter">
-          {post.date}
-        </div>
+      <h1>{post.title}</h1>
+      <div>
+        <div>{post.date}</div>
         <div>
           <div dangerouslySetInnerHTML={{ __html: post.body.html }} />
         </div>
