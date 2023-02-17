1e17892243d2073c71374e9011b765fbef7a0832
SameerJadav <sameerjadav010@gmail.com>
2023-02-17 18:26:02 +0530

    Added code theme

--- contentlayer.config.ts
+++ contentlayer.config.ts
@@ -1,4 +1,6 @@
 import { defineDocumentType, makeSource } from 'contentlayer/source-files';
+import remarkGfm from 'remark-gfm';
+import rehypePrettyCode from 'rehype-pretty-code';
 
 const Post = defineDocumentType(() => ({
   name: 'Post',
@@ -32,4 +34,28 @@ const Post = defineDocumentType(() => ({
 export default makeSource({
   contentDirPath: 'posts',
   documentTypes: [Post],
+  markdown: {
+    remarkPlugins: [remarkGfm],
+    rehypePlugins: [
+      [
+        rehypePrettyCode,
+        {
+          theme: 'one-dark-pro',
+          onVisitLine(node) {
+            // Prevent lines from collapsing in `display: grid` mode, and allow empty
+            // lines to be copy/pasted
+            if (node.children.length === 0) {
+              node.children = [{ type: 'text', value: ' ' }];
+            }
+          },
+          onVisitHighlightedLine(node) {
+            node.properties.className.push('line--highlighted');
+          },
+          onVisitHighlightedWord(node) {
+            node.properties.className = ['word--highlighted'];
+          },
+        },
+      ],
+    ],
+  },
 });
