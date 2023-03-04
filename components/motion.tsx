ccb89b79532c45ee2b00e64aa6ddec1eccbf42f4
SameerJadav <sameerjadav010@gmail.com>
2023-03-04 11:24:40 +0530

    Draw animation in logo shape

---
+++ components/motion.tsx
@@ -0,0 +1,22 @@
+export const icon = {
+  hidden: {
+    pathLength: 0,
+  },
+  visible: {
+    pathLength: 1,
+  },
+};
+
+export const draw = {
+  hidden: { pathLength: 0, opacity: 0 },
+  visible: () => {
+    return {
+      pathLength: 1,
+      opacity: 1,
+      transition: {
+        pathLength: { delay: 0.5, type: 'spring', duration: 1.5, bounce: 0 },
+        opacity: { delay: 0.5, duration: 0.01 },
+      },
+    };
+  },
+};
